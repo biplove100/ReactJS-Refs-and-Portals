852acc86739568bca8ee30ffc354bb8870dcbbb2
Biplove Singh <biplovesinghrajpute.bsr1@gmail.com>
2026-02-18 15:31:22 +0530

    Handled two way binding using states only

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { useRef } from "react";
+
 export default function Player() {
+  
+
+  const [userName, setUserName] = useState('');
+  const [buttonClicking, setButtonClicking] = useState(false);
+
+  function handleInputText(inputText){
+    setUserName(inputText.target.value);
+    console.log(userName);
+  }
+
+  function handleClick(){
+    setButtonClicking(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {buttonClicking ? userName : "unknown User"}</h2>
+      
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleInputText} value={userName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );

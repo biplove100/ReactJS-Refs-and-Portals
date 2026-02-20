946cee1db4b8022b290502a35dcca4fc23d043a6
Biplove Singh <biplovesinghrajpute.bsr1@gmail.com>
2026-02-20 10:06:43 +0530

    useReference hook is used in the input element to print the useName

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const [userName, setUserName] = useState('');
+  const playerName = useRef();
+
+  function handleClick(){
+    console.log("Button is Clicked");
+    setUserName(playerName.current.value);
+
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {userName !="" ? userName : "unknown User" }</h2>
+      
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );

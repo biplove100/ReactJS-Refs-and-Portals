e3d9329cdf87d818a1b9f65e971b58bb0c923c26
Biplove Singh <biplovesinghrajpute.bsr1@gmail.com>
2026-02-23 11:40:42 +0530

    Completed till References, used to output username on main screen

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,21 @@
+import { useState, useRef } from "react";
+
+
 export default function Player() {
+  const [inputText, setInputText] = useState('');
+  const playerName = useRef();
+
+  function handleClick(){
+    setInputText(playerName.current.value);
+    console.log(inputText);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {inputText!='' ? inputText : "Unknown User"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick} >Set Name</button>
       </p>
     </section>
   );
